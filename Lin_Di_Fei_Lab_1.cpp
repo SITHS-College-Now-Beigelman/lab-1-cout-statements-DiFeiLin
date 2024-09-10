5a473cb8f0ae5bb8bb6be3477cb11cc4427cd132
DiFeiLin <difeil2@nycstudents.net>
2024-09-10 17:14:12 -0400

    added proper formatting

--- Lin_Di_Fei_Lab_1.cpp
+++ Lin_Di_Fei_Lab_1.cpp
@@ -1,15 +1,16 @@
-/*
-Di Fei Lin
-Lab 1 
-9/10
-comment output in program, comments in program, submitted via github and screenshot of the whole screen
-*/
+
+//Di Fei Lin
+//Lab 1 
+//9/10
+//comment output in program, comments in program, submitted via github and screenshot of the whole screen
+
 
 #include <iostream>
 
 using namespace std;
 
 int main(){
+    
     cout<<"##############################################################################"<<endl;
     cout<<"#                                                                            #"<<endl;
     cout<<"# Ways to access the Task Manager on your Windows computer:                  #"<<endl;
@@ -21,10 +22,21 @@ int main(){
     cout<<"#    Type \"Task Manager\" in the Windows Start menu search                    #"<<endl;
     cout<<"#                                                                            #"<<endl;
     cout<<"##############################################################################"<<endl;
-    return 0;
 
+    return 0;
 } 
 /*
+##############################################################################
+#                                                                            #
+# Ways to access the Task Manager on your Windows computer:                  #
+#                                                                            #
+#    Press the key combination Ctrl + Shift + Escape                         #
+#                                                                            #
+#    Press the key combination Ctrl + Alt + Delete and select "Task Manager" #
+#                                                                            #
+#    Type "Task Manager" in the Windows Start menu search                    #
+#                                                                            #
+##############################################################################
 line 13, establishes the beginning wall of hastags 
 line 14, spacing adhering to instructions
 line 15,prints first line
@@ -32,10 +44,9 @@ line 16,spacing adhering to instructions
 line 17,prints second line + indent
 line 18,spacing adhering to instructions
 line 19,prints third line + indent
-line 20,spacing adhering to instructions
+line 20,spacing adhering to instruction
 line 21,prints fourth line + indent
 line 22,spacing adhering to instructions
 line 23, establishes the ending wall of hastags 
 line 24,returning and printing the function
-
 */
